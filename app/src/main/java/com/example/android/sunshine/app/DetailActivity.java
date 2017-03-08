61d21834a0bd086c1e635c0ad615294b4741f806
cebindani <danielecebin@hotmaill.com>
2017-03-08 19:52:52 -0300

    Create SettingsActivity and launch from MainActivity and DetailActivity

--- app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
@@ -36,6 +36,7 @@ public class DetailActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
         return super.onOptionsItemSelected(item);
