0066633ef6ad52e10510327037c57a2966618b54
cebindani <danielecebin@hotmail.com>
2017-03-13 22:46:44 -0300

    Add a ShareActionProviders to a DetailActivity fragment to share a forecast detail

--- app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
@@ -3,15 +3,20 @@ package com.example.android.sunshine.app;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +26,7 @@ public class DetailActivity extends AppCompatActivity {
 
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new DetailActivityFragment())
+                    .add(R.id.container, new DetailFragment())
                     .commit();
         }
     }
@@ -29,7 +34,9 @@ public class DetailActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.detail, menu);
-        return true;
+
+
+        return true; // Return true to display menu
     }
 
     @Override
@@ -38,14 +45,21 @@ public class DetailActivity extends AppCompatActivity {
         if (id == R.id.action_settings) {
             startActivity(new Intent(this, SettingsActivity.class));
             return true;
+        } else if (id == R.id.action_share) {
+
+
         }
         return super.onOptionsItemSelected(item);
     }
 
 
-    public static class DetailActivityFragment extends Fragment {
+    public static class DetailFragment extends Fragment {
+        public static final String SUNSHINE_APP_HASHTAG = " #SunshineApp";
+        private static final String LOG_TAG = DetailFragment.class.getSimpleName();
+        private String mForecastData;
 
-        public DetailActivityFragment() {
+        public DetailFragment() {
+            setHasOptionsMenu(true);
         }
 
         @Override
@@ -55,10 +69,40 @@ public class DetailActivity extends AppCompatActivity {
             Intent intent = getActivity().getIntent();
             View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
             if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
-                String forecastData = intent.getStringExtra(Intent.EXTRA_TEXT);
-                ((TextView) rootView.findViewById(R.id.detail_text)).setText(forecastData);
+                mForecastData = intent.getStringExtra(Intent.EXTRA_TEXT);
+                ((TextView) rootView.findViewById(R.id.detail_text)).setText(mForecastData);
             }
             return rootView;
+        }
+
+        @Override
+        public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+            inflater.inflate(R.menu.detailfragment, menu);
+
+            // Retrieve the share menu item
+            MenuItem menuItem = menu.findItem(R.id.action_share);
+
+            // Fetch and store ShareActionProvider
+            ShareActionProvider mShareActionProvider =
+                    (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+
+            //Attach an intent to this ShareActionProvider.  You can update this at any time
+            // like when the user selects a new piece of data they might like to share.
+            if (mShareActionProvider != null) {
+                mShareActionProvider.setShareIntent(createShareForecastIntent());
+            } else {
+                Log.d(LOG_TAG, "Share Action Provider is null");
+            }
+
+        }
+
+        private Intent createShareForecastIntent() {
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+
+            shareIntent.putExtra(Intent.EXTRA_TEXT, mForecastData + SUNSHINE_APP_HASHTAG);
+            shareIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+            shareIntent.setType("text/plain");
+            return shareIntent;
         }
     }
 
